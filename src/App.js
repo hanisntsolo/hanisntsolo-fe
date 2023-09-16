6bd366b84e6e5382cf7b3fff37f4a4c815f7ec1f
Dhirendra Pratap Singh <36436712+hanisntsolo@users.noreply.github.com>
2023-09-16 20:28:11 +0000

    main

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         <code>This application is running on github pages!!</code>
-        <code>Code pushed from site branch in repository!</code>
+        <code>Code pushed from main branch in repository!</code>
         </p>
         <a
           className="App-link"
